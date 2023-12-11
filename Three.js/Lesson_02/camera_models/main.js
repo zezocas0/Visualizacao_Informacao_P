f1f54aafcee39cc306db533b337aaada9856fca7
unknown <ricardofantunes@ua.pt>
2023-12-11 11:44:45 +0000

    Up

---
+++ Three.js/Lesson_02/camera_models/main.js
@@ -0,0 +1,27 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const aspect = width/height;
+const camera = new THREE.OrthographicCamera(-3 * aspect, 3 * aspect, 3, -3, 1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	renderer.render( scene, camera );
+}
+
+animate();
