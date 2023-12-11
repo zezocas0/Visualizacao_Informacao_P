9679273906b71c82a1be9335d294ee73a6852d7f
unknown <ricardofantunes@ua.pt>
2023-12-11 11:48:08 +0000

    Update

--- Three.js/Lesson_02/camera_models/main.js
+++ Three.js/Lesson_02/camera_models/main.js
@@ -24,4 +24,10 @@ function animate() {
 	renderer.render( scene, camera );
 }
 
+window.addEventListener('resize', function () {				       
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	});
+
 animate();
